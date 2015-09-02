4efc73fd1537a654903881e5df11ffe254bf1934
Mark Hiner <hinerm@gmail.com>
2015-09-02 15:04:12 -0500

    Tutorial 2: use @File instead of JFileChooser
    This avoids deadlocks!

--- src/main/resources/script_templates/Tutorials/02_-_Load_and_Display_Dataset.py
+++ src/main/resources/script_templates/Tutorials/02_-_Load_and_Display_Dataset.py
@@ -1,20 +1,9 @@
 # @DatasetService ds
 # @UIService ui
+# @File file
 
-from java.io import File
-from javax.swing import JFileChooser
-from net.imagej import Dataset
-from net.imagej import ImageJ
+# load the dataset
+dataset = ds.open(file.getAbsolutePath())
 
-# ask the user for a file to open
-chooser = JFileChooser()
-returnVal = chooser.showOpenDialog(None)
-
-if returnVal == JFileChooser.APPROVE_OPTION:
-	theFile = chooser.getSelectedFile()
-
-	# load the dataset
-	dataset = ds.open(theFile.getAbsolutePath())
-
-	# display the dataset
-	ui.show(dataset)
+# display the dataset
+ui.show(dataset)
