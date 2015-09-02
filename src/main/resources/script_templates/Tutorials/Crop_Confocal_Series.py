57a2a5fd984c1a926ef17463437cad10e567cd41
Brian Northan <bnorthan@gmail.com>
2015-09-02 14:38:42 -0400

    set names for new ImgPlus

--- src/main/resources/script_templates/Tutorials/Crop_Confocal_Series.py
+++ src/main/resources/script_templates/Tutorials/Crop_Confocal_Series.py
@@ -1,6 +1,7 @@
 # @OpService ops
 # @net.imagej.Dataset data
 # @UIService ui
+# @DisplayService display
 
 # to run this tutorial run 'file->Open Samples->Confocal Series' and make sure that
 # confocal-series.tif is the active image
@@ -21,21 +22,25 @@ cLen = data.dimension(data.dimensionIndex(Axes.CHANNEL));
 
 # crop a channel
 c0=ops.image().crop(img, Intervals.createMinMax(0, 0, 0,0,xLen-1, yLen-1, 0, zLen-1))
+c0.setName("c0")
 
 # crop both channels at z=12
 z12=ops.image().crop(img, Intervals.createMinMax(0,0,0,12, xLen-1, yLen-1, cLen-1, 12))
+z12.setName("z12")
 
 # crop channel 0 at z=12
 c0z12=ops.image().crop(img, Intervals.createMinMax(0,0,0,12, xLen-1, yLen-1, 0, 12))
+c0z12.setName("c0z12")
 
 # crop an roi at channel 0, z=12
 roiC0z12=ops.image().crop(img, Intervals.createMinMax(150,150,0,12, 200, 200, 0, 12))
+roiC0z12.setName("roiC0z12")
 
 # display all the cropped images
-ui.show("C0", c0)
-ui.show("z12", z12)
-ui.show("C0z12", c0z12)
-ui.show("roiC0z12", roiC0z12)
+ui.show(c0)
+ui.show(z12)
+ui.show(c0z12)
+ui.show(roiC0z12)
 
 
 
