a7a5ab7969bbd1dac4bee6ca4d7f6ed00e425edf
Mark Hiner <hinerm@gmail.com>
2016-02-12 08:52:12 -0600

    Add simple convolution example

---
+++ src/main/resources/script_templates/Tutorials/Simple_Convolution.groovy
@@ -0,0 +1,19 @@
+// @OpService ops
+// @Dataset inputData
+// @OUTPUT ImgPlus(label="Filtered") filtered
+// @OUTPUT ImgPlus(label="Convolved") result
+
+// This script takes an input image, applies a gaussian filter
+// to it, and then performs convolution with a custom kernel.
+
+// Convert the input image
+img32 = ops.convert().int32(inputData.getImgPlus())
+
+// Apply the gaussian filter
+filtered=ops.filter().gauss(img32, [4.0,4.0])
+
+// Create the kernel
+kernel=ops.create().kernel([[0,1,0],[1,-4,1],[0,1,0]])
+
+// Perform convolution
+result=ops.filter().convolve(filtered, kernel)
