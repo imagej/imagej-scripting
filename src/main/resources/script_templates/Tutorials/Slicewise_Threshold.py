1925c51e2efb8fde59e963cbf97b100c719f293b
Stefan Helfrich <stefan.helfrich@googlemail.com>
2016-03-23 08:54:41 +0100

    Slicewise_Threshold.py: fix bug and clean up
    Calling OpService.ops(Op, Dataset) throws an
    UnsupportedOperationException. Instead call it with the ImgPlus of a
    Dataset.

--- src/main/resources/script_templates/Tutorials/Slicewise_Threshold.py
+++ src/main/resources/script_templates/Tutorials/Slicewise_Threshold.py
@@ -1,39 +1,39 @@
 # @OpService ops
 # @Dataset data
-# @UIService ui
+# @OUTPUT ImgPlus thresholdedPlus
 
-# To run this tutorial, go to 'file>Open Samples>Confocal Series' and make sure that
-# confocal-series.tif is the active image
+# To run this tutorial, go to 'File>Open Samples>Confocal Series' and make sure
+# that confocal-series.tif is the active image
 
 from net.imagej.ops import Ops
 from net.imagej import ImgPlus
-from net.imglib2 import FinalDimensions
 from net.imglib2.type.logic import BitType
 from net.imagej.axis import Axes
 
 
 # first take a look at the size and type of each dimension
 for d in range(data.numDimensions()):
-	print "axis d: type: "+str(data.axis(d).type())+" length: "+str(data.dimension(d))
-
+	print "axis "+str(d)+": type: "+str(data.axis(d).type())+" length: "+str(data.dimension(d))
+	
 xDim = data.dimensionIndex(Axes.X)
 yDim = data.dimensionIndex(Axes.Y)
 zDim = data.dimensionIndex(Axes.Z)
-cDim = data.dimensionIndex(Axes.CHANNEL)
+cDim = data.dimensionIndex(Axes.CHANNEL) # Not used, just for reference
 
 # create the otsu op
-otsu=ops.op(Ops.Threshold.Otsu, data)
+otsu=ops.op(Ops.Threshold.Otsu, data.getImgPlus())
 
 # create memory for the thresholded image
 thresholded=ops.create().img(data.getImgPlus(), BitType())
 
-# call slice wise thresholde axis to process, in this case [0,1] means process the 
+# call threshold op slice-wise for the defined axes, in this case [xDim,yDim] means process the 
 # first two axes (x and y)
 ops.slicewise(thresholded, data.getImgPlus(), otsu, [xDim,yDim])
 
-# try again with [xDim, yDim, zDim] is the result different??  Why??
+# try again with [xDim, yDim, zDim] is the result different? Why?
 
 # create an ImgPlus using the thresholded img, copy meta data from the input
 thresholdedPlus=ImgPlus(thresholded, data.getImgPlus(), True)
 
-ui.show("thresholded", thresholdedPlus)
+# set a new name to avoid confusion
+thresholdedPlus.setName("Thresholded")
