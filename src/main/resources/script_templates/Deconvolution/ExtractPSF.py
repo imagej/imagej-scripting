3377d4c0c7a1af512acba5baf953b8c3b38747f0
Curtis Rueden <ctrueden@wisc.edu>
2022-06-16 19:12:44 -0500

    ExtractPSF: make the script work again
    The DeconvolveNamespace#richardsonLucy methods changed in a
    backwards-incompatible way, when the richardsonLucy ops became
    computers rather than hybrids.
    
    See: https://forum.image.sc/t/68424

--- src/main/resources/script_templates/Deconvolution/ExtractPSF.py
+++ src/main/resources/script_templates/Deconvolution/ExtractPSF.py
@@ -1,11 +1,12 @@
 #@ OpService ops
 #@ ImgPlus beads
-#@OUTPUT ImgPlus thresholded
-#@OUTPUT ImgPlus points
-#@OUTPUT ImgPlus psf
+#@output ImgPlus thresholded
+#@output ImgPlus points
+#@output ImgPlus psf
 
 from net.imglib2.algorithm.labeling.ConnectedComponents import StructuringElement
 from net.imglib2.roi.labeling import LabelRegions
+from net.imglib2.type.numeric.real import FloatType
 
 # reference
 # (note. there used to be good references on the Huygens web site, but that seems only accessible by password now)
@@ -22,38 +23,49 @@ from net.imglib2.roi.labeling import LabelRegions
 #    in position should "even out".
 
 # get dimensions of bead image
-xSize=beads.dimension(0)
-ySize=beads.dimension(1)
-zSize=beads.dimension(2)
+xSize = beads.dimension(0)
+ySize = beads.dimension(1)
+zSize = beads.dimension(2)
 
 # create an empty image to store the points
-points=ops.create().img([xSize, ySize, zSize])
+points = ops.create().img([xSize, ySize, zSize])
 
 # otsu threshold to find the beads
 thresholded = ops.threshold().otsu(beads)
 
 # call connected components to label each connected region
-labeling=ops.labeling().cca(thresholded, StructuringElement.FOUR_CONNECTED)
+labeling = ops.labeling().cca(thresholded, StructuringElement.FOUR_CONNECTED)
 
 # get the index image (each object will have a unique gray level)
-labelingIndex=labeling.getIndexImg()
+labelingIndex = labeling.getIndexImg()
 
 # get the collection of regions and loop through them
-regions=LabelRegions(labeling)
+regions = LabelRegions(labeling)
 
+randomAccess = points.randomAccess()
 for region in regions:
 
 	# get the center of mass of the region
-	center=region.getCenterOfMass()
+	center = region.getCenterOfMass()
 
 	# place a point at the bead centroid
-	randomAccess= points.randomAccess()
 	randomAccess.setPosition([long(center.getFloatPosition(0)), long(center.getFloatPosition(1)), long(center.getFloatPosition(2))])
 	randomAccess.get().setReal(255.0)
 
 # convert images to float
-beads32=ops.convert().float32(beads)
-points32=ops.convert().float32(points)
+beads32 = ops.convert().float32(beads)
+points32 = ops.convert().float32(points)
 
 # use "PSF Distilling" (reverse deconvolution) to solve for PSF
-psf=ops.deconvolve().richardsonLucy(beads32, points32, None, None, None, None, None, 30, False, True)
+image = beads32
+kernel = points32
+borderSize = None
+obfInput = None
+obfKernel = None
+outType = FloatType()
+fftType = None
+maxIterations = 30
+nonCirculant = False
+accelerate = True # The default is False.
+psf = ops.create().img(image, outType)
+ops.deconvolve().richardsonLucy(psf, beads32, points32, borderSize, obfInput, obfKernel, outType, fftType, maxIterations, nonCirculant, accelerate)
